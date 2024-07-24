da295dfebbefe3b7c6b5aa207766f9c9871d03b1
RjSup <19141558@brookes.ac.uk>
2024-07-24 17:09:57 +0100

    -- added contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 const Footer = () => {
-    let currentDate = new Date().toJSON().slice(0, 10);
-
-    return <footer>{`© Ryan James | ${currentDate}`}</footer>;
-}
-
-export default Footer;
+    const currentDate = new Date();
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const year = currentDate.getFullYear();
+  
+    const formattedDate = `${day}-${month}-${year}`;
+  
+    return <footer>{`© Ryan James | ${formattedDate}`}</footer>;
+  };
+  
+  export default Footer;
+  
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,10 +1,52 @@
 import '../pages/Contact.css';
+import { useState } from 'react';
 
 
 const Contact = () => {
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "acc6292c-a3a4-4eb3-97f4-4001fe1dfdc0");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
+
     return (
         <div className='contactdiv'>
             <h1>Contact Me</h1>
+            <div>
+                <form onSubmit={onSubmit}>
+                    <input type="hidden" name="access_key" value="acc6292c-a3a4-4eb3-97f4-4001fe1dfdc0"></input>    
+                    <label for="name">Name: </label>
+                    <input type="text" name="name" required/>
+                    <label for="email">Email: </label>
+                    <input type="email" name="email" required/>
+                    <label for="message">Message: </label>
+                    <textarea name="message" rows={4} cols={200} required></textarea>
+
+                    <button type="submit">Submit Form</button>
+                </form>
+                <span>{result}</span>
+
+            </div>
         </div>
     )
 };
